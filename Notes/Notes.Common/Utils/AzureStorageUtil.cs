02d409e54db4b6984e546a86832715750f52bebe
Nick Lam <lkn.cntt@gmail.com>
2016-10-25 14:34:20 +0800

    Add some utils

---
+++ Notes/Notes.Common/Utils/AzureStorageUtil.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.RetryPolicies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Common.Utils
+{
+    public class AzureStorageUtil
+    {
+        public static async Task<string> UploadToAzureStorage(string connectionString, string containerName, string fileName, byte[] data)
+        {
+            var storageAccount = CloudStorageAccount.Parse(connectionString);
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            blobClient.DefaultRequestOptions.RetryPolicy = new LinearRetry(TimeSpan.FromSeconds(3), 3);
+            var blobContainer = blobClient.GetContainerReference(containerName);
+            var blob = blobContainer.GetBlockBlobReference(fileName);
+            await blob.UploadFromByteArrayAsync(data, 0, data.Length);
+            return blob.Uri.ToString();
+        }
+    }
+}
---
+++ Notes/Notes.Common/Utils/EmailUtil.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Common.Utils
+{
+    public class EmailUtil
+    {
+        public static async Task SendEmail(string recipients, string subject, string body, IEnumerable<Attachment> attachments)
+        {
+            using (var smtpClient = new SmtpClient())
+            {
+                MailMessage mm = new MailMessage();
+                mm.To.Add(recipients);
+                mm.Subject = subject;
+                mm.Body = body;
+
+                mm.IsBodyHtml = true;
+
+                if (attachments != null && attachments.Any())
+                {
+                    foreach (var attachment in attachments)
+                    {
+                        mm.Attachments.Add(attachment);
+                    }
+                }
+
+                await smtpClient.SendMailAsync(mm);
+            }
+        }
+    }
+}
