e20dc25f6bf13cb55e2d86cc112906d39cd33881
Nick Lam <lkn.cntt@gmail.com>
2016-10-25 15:00:40 +0800

    Add Models

---
+++ Notes/Notes.Common/Models/Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Common.Models.Entities
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset ModifiedAt { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ Notes/Notes.Common/Models/ValueObjects/Price.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Common.Models.ValueObjects
+{
+    public class Price
+    {
+        public string Currency { get; set; }
+        public decimal Amount { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Currency} {Amount}";
+        }
+    }
+}
